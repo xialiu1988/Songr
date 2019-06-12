3287b9c38500c372db7c52bfb05f0d21977fb2c2
xialiu1988 <liuxia198896@gmail.com>
2019-06-12 09:18:59 -0700

    initial commit

---
+++ src/main/java/com/xl88/cf401d4/lab/Songr/SongrApplication.java
@@ -0,0 +1,13 @@
+package com.xl88.cf401d4.lab.Songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
